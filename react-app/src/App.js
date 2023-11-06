7a3b3c9c36110c8833b7a4f3c35f9992542b366e
sriram <sriram.is21@bmsce.ac.in>
2023-11-06 18:42:12 +0530

    incorporated dark mode into text utils

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,30 @@
-
 import './App.css';
+// import Accordition from './components/Accordition';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
+import React, { useState } from 'react'
+
+
+
 function App() {
+  const [mode,setmode]= useState("light");
+
+  const togglemode=()=>{
+    console.log("mode changed")
+    if(mode==="light")
+      {setmode("dark")
+      document.body.style.backgroundColor="grey"}
+    else
+      {setmode("light")
+      document.body.style.backgroundColor="white"}
+  }
+
   return (
     <>
-    <Navbar title="textutils" navbar1="first" navbar2="second"/>
-    <Textform startingtext="enter text"/>
+    <Navbar title="textutils" navbar1="first" navbar2="second" mode={mode} togglemode={togglemode}/>
+    <Textform startingtext="enter text" mode={mode} togglemode={togglemode}/>
+    {/* <Accordition/> */}
     </>
   );
 }
--- react-app/src/components/Textform.js
+++ react-app/src/components/Textform.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 export default function Textform(props) {
     const [text,settext]= useState('');
+
     const touppercasefn=()=>
     {   
         // string var=text.toUpperCase
@@ -15,19 +16,30 @@ export default function Textform(props) {
     const onchangefn=(event)=>{
         settext(event.target.value)
     }
+    const copytoclipboard=()=>{
+        navigator.clipboard.writeText(text);
+    }
   return (
-        <div className='container my-3' >
+        <div className='container my-3'style={{
+        backgroundColor: props.mode==="light"?"white":"black",
+        color:props.mode==="light"?"black":"white"
+        }} >
         <div className="form-group">
             <h1>
                 {props.startingtext}
             </h1>
-        <textarea className="form-control" value={text} onChange={onchangefn} placeholder="Recipient's username" id="exampleFormControlTextarea1" rows="8"></textarea>
+        <textarea className="form-control " style={{
+        backgroundColor: props.mode==="light"?"white":"grey",
+        color:props.mode==="light"?"black":"white"
+        }} value={text} onChange={onchangefn} placeholder="Recipient's username" id="exampleFormControlTextarea1" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-3" onClick={touppercasefn}>Convert to upper Case</button>
         <button className="btn btn-primary mx-3" onClick={addnamefn}>Hello HOmie</button>
+        <button className="btn btn-primary mx-3" onClick={copytoclipboard}>clipboardcopy</button>
+
         <div className="container">
             <h1>summary</h1>
-            <p>{text.split("/s/").length}+words+{text.length}+letters
+            <p>{text.split(/[ ]+/).length}+words+{text.length}+letters
             </p>
         </div>
         </div>
