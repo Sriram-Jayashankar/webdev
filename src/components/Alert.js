a156ecba37aaeb151a8e03bf8dd7f7984ec50d6a
Sriram Jayashankar <sriramjayashankar@gmail.com>
2023-11-08 23:33:55 +0530

    final commit for the proj nothing major

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,7 +5,7 @@ export default function Alert(props) {
 if(props.alert)
   {return (
     
-    <div class={`alert alert-${props.alert.alerttype} alert-dismissible fade show`} role="alert">
+    <div class={`alert alert-${props.alert.alerttype} alert-dismissible fade show`} role="alert" style={{position:'absolute',alignContent:'center'}}>
         <strong>{props.alert.alerttype}</strong>{props.alert.alerttitle}
     </div>
   )}
